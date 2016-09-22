7dfdff623c60ca66da0d57786d795052a90c8963
Ash Whiting <awhiting@thisispegasus.co.uk>
2016-09-22 09:45:32 +0100

    moved everything out of the root

--- web/wp-config.php
+++ web/wp-config.php
@@ -1,4 +1,61 @@
 <?php
+ini_set( 'display_errors', 0 );
+
+// ===================================================
+// Load database info and local development parameters
+// ===================================================
+if ( file_exists( dirname( __FILE__ ) . '/../production-config.php' ) ) {
+    define( 'WP_LOCAL_DEV', false );
+    include( dirname( __FILE__ ) . '/../production-config.php' );
+} else {
+    define( 'WP_LOCAL_DEV', true );
+    include( dirname( __FILE__ ) . '/../local-config.php' );
+}
+
+// ========================
+// Custom Content Directory
+// ========================
+define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/content' );
+define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content' );
+define( 'DISALLOW_FILE_EDIT', true );
+
+// ================================================
+// You almost certainly do not want to change these
+// ================================================
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+
+// ================================
+// Language
+// Leave blank for American English
+// ================================
+define( 'WPLANG', '' );
+
+// ======================
+// Hide errors by default
+// ======================
+define( 'WP_DEBUG_DISPLAY', false );
+define( 'WP_DEBUG', false );
+
+// =========================
+// Disable automatic updates
+// =========================
+define( 'AUTOMATIC_UPDATER_DISABLED', false );
+define('FS_METHOD', 'direct');
+
+// =======================
+// Load WordPress Settings
+// =======================
+$table_prefix  = 'wp_';
+
+if ( ! defined( 'ABSPATH' ) ) {
+    define( 'ABSPATH', dirname( __FILE__ ) . '/wp/' );
+}
+require_once( ABSPATH . 'wp-settings.php' );
+
+
+
+<?php
 /**
  * The base configurations of the WordPress.
  *
@@ -20,6 +77,7 @@
  */
 
 define( 'DISALLOW_FILE_EDIT', true );
+define('FS_METHOD', 'direct');
 
 // STEP 1: Edit database settings below
 
@@ -115,7 +173,7 @@ define('WPLANG', '');
  * in their development environments.
  */
 define('WP_DEBUG', false);
-define('FS_METHOD', 'direct');
+
 
 /* That's all, stop editing! Happy blogging. */
 
